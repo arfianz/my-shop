731ebc4d923e90263a84f82c6d00b2c08db41883
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-19 10:39:49 +0700

    Setup redux toolkit
    add several redux packages to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.scss';
+import { Provider } from 'react-redux';
+import store from '../store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ store/cartSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: 'hello',
+  reducers: {},
+});
+
+export default cartSlice.reducer;
