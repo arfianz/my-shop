f969730c5b93bd7df6e6f7583fe08a8366efb0c2
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-19 20:06:04 +0700

    Footer component - Part 2 -
    add social link and icon, and add newsletter component to footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,6 @@
 import Links from './Links';
+import Socials from './Socials';
+import Newsletter from './Newsletter';
 import styles from './styles.module.scss';
 
 export default function Footer() {
@@ -6,6 +8,8 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
+        <Socials />
+        <Newsletter />
       </div>
     </footer>
   );
