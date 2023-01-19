1e091f1b2a8b6894533a8bd41b41064f4122564e
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-19 10:09:09 +0700

    Database testing
    connection test to mongodb cluster

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from '../../utils/db';
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  db.connectDb();
+  db.disconnectDb();
+  res.status(200).json({ name: 'John Doe' });
 }
