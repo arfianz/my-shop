5c29eda7af8eecca83b91329412fa2e384764465
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-20 17:20:22 +0700

    Authentication - Part 19 -
    add send mail function for user validation

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -4,6 +4,8 @@ import db from '../../../utils/db';
 import User from '../../../models/User';
 import { validateEmail } from '../../../utils/validation';
 import { createActivationToken } from '../../../utils/tokens';
+import { sendEmail } from '../../../utils/sendEmails';
+import { activateEmailTemplate } from '../../../emails/activateEmailTemplate';
 
 const handler = nc();
 
@@ -24,16 +26,21 @@ handler.post(async (req, res) => {
     if (password.length < 6) {
       return res
         .status(400)
-        .json({ message: 'Password must be at least 6 characters.' });
+        .json({ message: 'Password must be atleast 6 characters.' });
     }
     const cryptedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({ name, email, password: cryptedPassword });
+
     const addedUser = await newUser.save();
     const activation_token = createActivationToken({
       id: addedUser._id.toString(),
     });
     const url = `${process.env.BASE_URL}/activate/${activation_token}`;
-    res.send(url);
+    sendEmail(email, url, '', 'Activate your account.', activateEmailTemplate);
+    await db.disconnectDb();
+    res.json({
+      message: 'Register success! Please activate your email to start.',
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
