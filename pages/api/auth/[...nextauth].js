65987383d7afd79e745313ab04c8681ddd0c502a
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-20 09:51:11 +0700

    Authentication - Part 7 -
    add auth0 oauth for sign-in method

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import TwitterProvider from 'next-auth/providers/twitter';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
+import Auth0Provider from 'next-auth/providers/auth0';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from './lib/mongodb';
 
@@ -26,6 +27,11 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
     }),
   ],
   pages: {
