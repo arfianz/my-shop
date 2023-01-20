29466a4f504a9abb881ff4e2c251f40a5eb34d45
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-20 13:35:56 +0700

    Authentication - Part 10 -
    customize signin page with additional form using Formik and Yup

--- components/inputs/loginInput/index.js
+++ components/inputs/loginInput/index.js
@@ -2,10 +2,17 @@ import styles from './styles.module.scss';
 import { BiUser } from 'react-icons/bi';
 import { SiMinutemailer } from 'react-icons/si';
 import { IoKeyOutline } from 'react-icons/io5';
+import { useField } from 'formik';
 
-export default function LoginInput({ icon, placeholder }) {
+export default function LoginInput({ icon, placeholder, ...props }) {
+  const [field, meta] = useField(props);
+
   return (
-    <div className={styles.input}>
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error ? styles.error : ''
+      }`}
+    >
       {icon == 'user' ? (
         <BiUser />
       ) : icon == 'email' ? (
@@ -15,7 +22,13 @@ export default function LoginInput({ icon, placeholder }) {
       ) : (
         ''
       )}
-      <input type='text' placeholder={placeholder} />
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
     </div>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -6,8 +6,29 @@ import Link from 'next/link';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import LoginInput from '../components/inputs/loginInput';
+import { useState } from 'react';
+
+const initialvalues = {
+  login_email: '',
+  login_password: '',
+};
 
 export default function signin() {
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required('Email address is required.')
+      .email('Please enter a valid email address.'),
+    login_password: Yup.string().required('Please enter a password'),
+  });
+
+  console.log(user);
+
   return (
     <>
       <Header country='Indonesia' />
@@ -26,10 +47,30 @@ export default function signin() {
             <p>
               Get access to one of the best eCommerce services in the world.
             </p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
-                  <LoginInput icon='email' placeholder='Email Address' />
+                  <LoginInput
+                    type='text'
+                    name='login_email'
+                    icon='email'
+                    placeholder='Email Address'
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type='password'
+                    name='login_password'
+                    icon='password'
+                    placeholder='Password'
+                    onChange={handleChange}
+                  />
                 </Form>
               )}
             </Formik>
