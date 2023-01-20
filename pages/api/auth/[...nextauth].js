60dc30bfdad337467813ba6431422c730701b0f7
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-20 07:34:22 +0700

    Authentication - Part 3 -
    add github oauth for sign-in method

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import AppleProvider from 'next-auth/providers/apple';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from './lib/mongodb';
 
@@ -22,9 +23,13 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
   pages: {
-    signIn: '/signin',
+    // signIn: '/signin',
   },
   session: {
     strategy: 'jwt',
