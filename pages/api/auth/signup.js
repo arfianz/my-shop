2a0d9f361027dc518ec43f7a852b8351b569a2fa
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-20 16:35:27 +0700

    Authentication - Part 16 -
    add package to enrypt password and creating token for user activation

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,9 @@
 import nc from 'next-connect';
+import bcrypt from 'bcrypt';
 import db from '../../../utils/db';
+import User from '../../../models/User';
 import { validateEmail } from '../../../utils/validation';
+import { createActivationToken } from '../../../utils/tokens';
 
 const handler = nc();
 
@@ -14,6 +17,23 @@ handler.post(async (req, res) => {
     if (!validateEmail(email)) {
       return res.status(400).json({ message: 'Invalid email.' });
     }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'This email already exist.' });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: 'Password must be at least 6 characters.' });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    console.log(activation_token);
+    res.send(activation_token);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
