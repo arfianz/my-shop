64385966b3975b550d32ea2cc8129e9dec7ca39e
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-21 13:50:14 +0700

    Home page - Part 5 -
    customizing main menu component

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,12 +1,13 @@
 import styles from './styles.module.scss';
 import MainSwiper from './swiper';
 import MainOffers from './offers';
+import MainMenu from './menu';
 
 export default function Main() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>This is Header</div>
-      <div className={styles.menu}>This is Menu</div>
+      <MainMenu />
       <MainSwiper />
       <MainOffers />
       <div className={styles.user}>This is User</div>
