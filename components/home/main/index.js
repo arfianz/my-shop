cc967764c59b1ffab5252d86fa6dcb59688acc0e
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-21 12:35:01 +0700

    Home page - Part 2 -
    customizing swiper component using swiper-js

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,11 +1,12 @@
 import styles from './styles.module.scss';
+import MainSwiper from './swiper';
 
 export default function index() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>This is Header</div>
       <div className={styles.menu}>This is Menu</div>
-      <div className={styles.swiper}>This is Swiper</div>
+      <MainSwiper />
       <div className={styles.offers}>This is Offers</div>
       <div className={styles.user}>This is User</div>
     </div>
