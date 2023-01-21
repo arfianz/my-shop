4813960c13a1937e8695658ffa9acfbfd072cf86
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-21 19:23:08 +0700

    Home page - Part 13 -
    customizing category card component

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,23 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Main from '../components/home/main';
 import FlashDeals from '../components/home/flashDeals';
+import Category from '../components/home/category';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import {
+  gamingSwiper,
+  homeImprovSwiper,
+  women_accessories,
+  women_dresses,
+  women_shoes,
+  women_swiper,
+} from '../data/home';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Home({ country }) {
   // console.log(country);
   const { data: session } = useSession();
+  const isMedium = useMediaQuery({ query: '(max-width:850px)' });
+  const isMobile = useMediaQuery({ query: '(max-width:550px)' });
 
   return (
     <div>
@@ -20,6 +32,32 @@ export default function Home({ country }) {
         <div className={styles.container}>
           <Main />
           <FlashDeals />
+          <div className={styles.home__category}>
+            <Category
+              header='Dresses'
+              products={women_dresses}
+              background='#5a31f4'
+            />
+            {!isMedium && (
+              <Category
+                header='Shoes'
+                products={women_shoes}
+                background='#3c811f'
+              />
+            )}
+            {isMobile && (
+              <Category
+                header='Shoes'
+                products={women_shoes}
+                background='#3c811f'
+              />
+            )}
+            <Category
+              header='Accessories'
+              products={women_accessories}
+              background='#000'
+            />
+          </div>
         </div>
       </div>
       <Footer country={country} />
