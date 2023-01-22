657710818d5fc2b15bf10e14dfc7dd32f45d5f02
Hendra Arfianto <hendra.arfianto@gmail.com>
2023-01-23 06:48:30 +0700

    Product page - Part 12 -
    customizing product page, add review component

--- components/productPage/reviews/addReview.js
+++ components/productPage/reviews/addReview.js
@@ -4,12 +4,24 @@ import { useState } from 'react';
 
 export default function AddReview({ product }) {
   const [size, setSize] = useState('');
+  const [style, setStyle] = useState('');
+
   return (
     <div className={styles.reviews__add}>
-      <div className='flex.wrap'>
-        <div className='flex' style={{ gap: '10px' }}>
-          Size :
-          <Select property={size} text='Size' data={product.allSizes} />
+      <div className={`${styles.flex} ${styles.wrap}`}>
+        <div className={styles.flex} style={{ gap: '10px' }}>
+          <Select
+            property={size}
+            text='Size'
+            data={product.allSizes.filter((x) => x.size !== size)}
+            handleChange={setSize}
+          />
+          <Select
+            property={style}
+            text='Style'
+            data={product.colors.filter((x) => x !== style)}
+            handleChange={setStyle}
+          />
         </div>
       </div>
     </div>
--- components/productPage/reviews/select.js
+++ components/productPage/reviews/select.js
@@ -2,26 +2,61 @@ import styles from './styles.module.scss';
 import { IoArrowDown } from 'react-icons/io5';
 import { useState } from 'react';
 
-export default function Select({ property, text, data }) {
+export default function Select({ property, text, data, handleChange }) {
   // console.log(data);
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
   return (
     <div className={styles.select}>
-      <div className={styles.select__header}>
+      {text} :
+      <div
+        className={styles.select__header}
+        onMouseOver={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        style={{
+          background: `${
+            text == 'Style' && property.color && `${property.color}`
+          }`,
+        }}
+      >
         <span
-          classname={`flex ${styles.select__header_wrap}`}
-          style={{ padding: '0 5px' }}
+          classname={`${styles.flex} ${styles.select__header_wrap}`}
+          style={{
+            padding: '0 5px',
+          }}
         >
-          {property || `Select ${text}`}
+          {text == 'Size' ? (
+            property || `Select ${text}`
+          ) : text == 'Style' && property.image ? (
+            <img src={property.image} alt='' />
+          ) : (
+            'Select Style'
+          )}
           <IoArrowDown />
         </span>
         {visible && (
-          <ul className={styles.select__header_menu}>
+          <ul
+            className={styles.select__header_menu}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
             {data.map((item, i) => {
               if (text == 'Size') {
                 return (
-                  <li key={i}>
+                  <li key={i} onClick={() => handleChange(item.size)}>
                     <span>{item.size}</span>
+                  </li>
+                );
+              }
+              if (text == 'Style') {
+                return (
+                  <li
+                    key={i}
+                    onClick={() => handleChange(item)}
+                    style={{ backgroundColor: `${item.color}` }}
+                  >
+                    <span>
+                      <img src={item.image} alt='' />
+                    </span>
                   </li>
                 );
               }
